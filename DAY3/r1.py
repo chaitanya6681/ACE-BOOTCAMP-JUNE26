5de38aa5758e49333773502db62a8eff969ff9d8
Chaitanya <mail2mchaitanya@gmail.com>
2026-06-19 10:56:35 +0000

    Add deepcopy and shallow copy examples

---
+++ DAY3/r1.py
@@ -0,0 +1,3 @@
+list=[[6]*3 for i in range(3)]
+list[0][0]=5
+print(list)
--- DAY3/rough.py
+++ DAY3/rough.py
@@ -1,6 +1,7 @@
+import copy
 l=[[1,2,3],[1,1,1],[2,2,4]]
-l1=[[1,2,3],[1,1,1],[2,2,4]]
-print(l+l1)
-for i in range(len(l)):
-    for j in range(len(l)):
-        print(l[i][j]*l1[i][j],end=" ")
+sl=copy.copy(l)
+sl[0][0]=100
+print(sl)
+l[0][0]=200
+print(l)
